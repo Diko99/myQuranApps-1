9979f199028e80c729eba1c01dc4f07fe71f6112
Diko99 <cikodiko12@gmail.com>
2020-04-08 09:12:31 +0700

    Add files via upload

---
+++ precache-manifest.28e4267adf7dd0a59ae7878595c71171.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bd6b1eacc0b43c58ff65a8d9f8986a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cf247d5a5e518529407",
+    "url": "/static/css/main.04209ea6.chunk.css"
+  },
+  {
+    "revision": "de72474b171d47d9d0f7",
+    "url": "/static/js/2.cfe3c678.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cfe3c678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf247d5a5e518529407",
+    "url": "/static/js/main.8f945f08.chunk.js"
+  },
+  {
+    "revision": "ea4a8b6beb2d9195e3a2",
+    "url": "/static/js/runtime-main.aeb9047a.js"
+  },
+  {
+    "revision": "40f10a2eedcf1c54571c5bc36b6e587c",
+    "url": "/static/media/loading-bar.40f10a2e.svg"
+  }
+]);
